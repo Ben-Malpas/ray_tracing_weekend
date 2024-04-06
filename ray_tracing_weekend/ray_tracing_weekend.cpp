e12bded2b2209dfb467c8b507a84b9a681952671
Ben Malpas <benamalpas@gmail.com>
2024-04-06 02:06:37 +0100

    Set up CMake environment and repo
    This patch sets up the repo and CMake project/ environment which will
    document the development of a basic ray tracer render engine following
    the tutorial found below:
    
    https://raytracing.github.io/books/RayTracingInOneWeekend.html#overview

---
+++ ray_tracing_weekend/ray_tracing_weekend.cpp
@@ -0,0 +1,13 @@
+﻿// ray_tracing_weekend.cpp : Defines the entry point for the application.
+//
+
+#include "ray_tracing_weekend.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	return 0;
+}
