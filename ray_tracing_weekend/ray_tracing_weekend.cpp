9cb767456a92fc24933d3ff7418667039ae2f7c4
Ben Malpas <benamalpas@gmail.com>
2024-04-10 22:19:34 +0100

    Add a sphere
    This uses the geometry of a sphere to calculate if a ray hit it via
    direct calculation.

--- ray_tracing_weekend/ray_tracing_weekend.cpp
+++ ray_tracing_weekend/ray_tracing_weekend.cpp
@@ -9,9 +9,23 @@
 
 #include <iostream>
 
+bool hit_sphere(const Point3& center, double radius, const Ray& r)
+{
+	Vec3 oc = center - r.origin();
+	auto a = dot(r.direction(), r.direction());
+	auto b = -2.0 * dot(r.direction(), oc);
+	auto c = dot(oc, oc) - radius * radius;
+	auto discriminant = b * b - 4 * a * c;
+	return (discriminant >= 0);
+}
+
 Colour ray_colour(const Ray& r)
 {
-	return Colour(0, 0, 0); // returns black for now
+	if (hit_sphere(Point3(0, 0, -1), 0.5, r))
+	{
+		return Colour(1, 0, 0);
+	}
+
 	Vec3 unit_direction = unit_vector(r.direction());
 	auto a = 0.5 * (unit_direction.y() + 1.0);
 	return (1 - a) * Colour(1, 1, 1) + a * Colour(0.5, 0.7, 1);
