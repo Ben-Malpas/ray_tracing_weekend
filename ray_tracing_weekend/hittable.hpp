57c89b15b0a6092b22c565deffef27dffbd740cb
Ben Malpas <benamalpas@gmail.com>
2024-04-14 11:04:33 +0100

    Add an abstraction of hittable object
    A hittable object is something which interacts with a ray. A sphere is a
    concrete class of this abstract type.

---
+++ ray_tracing_weekend/hittable.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "ray.hpp"
+
+class HitRecord
+{
+public:
+	Point3 p;
+	Vec3 normal;
+	double t;
+};
+
+class Hittable
+{
+public:
+	virtual ~Hittable() = default;
+
+	virtual bool hit(const Ray & r, double ray_tmin, double ray_tmax, HitRecord & rec) const = 0;
+};
