dfb659353ac10ad4485767fcd5617471c7a3e773
Ben Malpas <benamalpas@gmail.com>
2024-04-17 19:30:12 +0100

    Add support for front/outer and back/inner face

--- ray_tracing_weekend/hittable.hpp
+++ ray_tracing_weekend/hittable.hpp
@@ -8,6 +8,14 @@ public:
 	Point3 p;
 	Vec3 normal;
 	double t;
+	bool front_face;
+
+	void set_face_normal(const Ray& r, const Vec3& normalised_outward_normal)
+	{
+		// Sets the hit record normal vector
+		front_face = dot(r.direction(), normalised_outward_normal);
+		normal = front_face ? normalised_outward_normal : -normalised_outward_normal;
+	}
 };
 
 class Hittable
