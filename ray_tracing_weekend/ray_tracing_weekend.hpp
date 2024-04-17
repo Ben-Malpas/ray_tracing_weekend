55b7362820e0231ce336fed58d0ceaceba7186fb
Ben Malpas <benamalpas@gmail.com>
2024-04-17 20:01:55 +0100

    Create a namespace RtWeekend

--- ray_tracing_weekend/ray_tracing_weekend.hpp
+++ ray_tracing_weekend/ray_tracing_weekend.hpp
@@ -5,4 +5,16 @@
 
 #include <iostream>
 
-// TODO: Reference additional headers your program requires here.
+namespace RtWeekend
+{
+    // Constants
+
+    const double infinity = std::numeric_limits<double>::infinity();
+    const double pi = 3.1415926535897932385;
+
+    // Utility Functions
+
+    inline double degrees_to_radians(double degrees) {
+        return degrees * pi / 180.0;
+    }
+}
